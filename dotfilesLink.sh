d6f3c5c9f2af4be90ef06dbca79614e6618f1f03
iori <ab.ioryz@gmail.com>
2015-04-02 22:32:55 +0900

    add ipython-settings

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -sf ~/.dotfiles/.gdbinit ~/.gdbinit
 ln -sf ~/.dotfiles/.pythonstartup ~/.pythonstartup
 ln -sf ~/.dotfiles/.xmodmaprc ~/.xmodmaprc
 ln -sf ~/.dotfiles/.zshenv ~/.zshenv
+ln -sf ~/.dotfiles/ipython-settings/00-first.py ~/.ipython/profile_default/startup/00-first.py
---
+++ ipython-settings/00-first.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import math
+import string
+import itertools
+import fractions
+import heapq
+import collections
+import re
+import array
+import bisect
+
+import numpy as np
+import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
