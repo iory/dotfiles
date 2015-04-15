dae437cdf95de3bb67d5bf92c71a855146a3e66e
iory <ab.ioryz@gmail.com>
2015-04-16 01:41:46 +0900

    add mahotas

--- ipython-settings/00-first.py
+++ ipython-settings/00-first.py
@@ -13,10 +13,13 @@ import bisect
 from see import see
 
 import cv2
+import numpy
 import numpy as np
+import scipy
 import scipy as sp
 import pandas as pd
 import matplotlib.pyplot as plt
+import mahotas as mh
 
 print("type()    -> analyze type of objects.")
 print("see()     -> extend dir().")
