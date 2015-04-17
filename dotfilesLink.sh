5cb8ffae90e6cf2354f76a02d196256d3af8747b
iory <ab.ioryz@gmail.com>
2015-04-18 04:34:45 +0900

    add global gitconfig and global gitignore

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,3 +5,5 @@ ln -sf ~/.dotfiles/.xmodmaprc ~/.xmodmaprc
 ln -sf ~/.dotfiles/.zshenv ~/.zshenv
 ln -sf ~/.dotfiles/ipython-settings/00-first.py ~/.ipython/profile_default/startup/00-first.py
 ln -sf ~/.dotfiles/.agignore ~/.agignore
+ln -sf ~/.dotfiles/.gitignore_global ~/.gitignore
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
