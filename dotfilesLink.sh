03fce9f453f971c691fbc968776c70759cf21941
iory <ab.ioryz@gmail.com>
2015-04-20 00:18:53 +0900

    add udev-settings for arduino

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,3 +7,7 @@ ln -sf ~/.dotfiles/ipython-settings/00-first.py ~/.ipython/profile_default/start
 ln -sf ~/.dotfiles/.agignore ~/.agignore
 ln -sf ~/.dotfiles/.gitignore_global ~/.gitignore
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+
+# for arduino
+sudo ln -sf ~/.dotfiles/udev-settings/10-arduino.rules /etc/udev/rules.d/10-arduino.rules
+sudo ln -sf ~/.dotfiles/udev-settings/11-arduino.rules /etc/udev/rules.d/11-arduino.rules
