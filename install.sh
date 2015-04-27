789b7fabe1e1766b0aabf2396c0688649df6b199
iory <ab.ioryz@gmail.com>
2015-04-27 13:13:51 +0900

    modified git@->https://

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 mkdir ~/.zsh/plugins
 cd ~/.zsh/plugins
-git clone git@github.com:zsh-users/zaw.git
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zaw.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
