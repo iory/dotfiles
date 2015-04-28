ce94aee24a9e19f87d8636e08377cf77e37f80db
iory <ab.ioryz@gmail.com>
2015-04-29 07:20:58 +0900

    delete unnecessary zshfile

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,7 +2,7 @@
 ln -sf ~/.dotfiles/.gdbinit ~/.gdbinit
 ln -sf ~/.dotfiles/.pythonstartup ~/.pythonstartup
 ln -sf ~/.dotfiles/.xmodmaprc ~/.xmodmaprc
-ln -sf ~/.dotfiles/.zshenv ~/.zshenv
+ln -sf ~/.dotfiles/.zsh/.zshenv ~/.zshenv
 
 # ipython settings
 if [ ! -e $HOME/.ipython/profile_default/startup ]; then
