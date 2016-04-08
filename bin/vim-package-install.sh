1654a4359b756575114d233361d576fe2ce43684
iory <ab.ioryz@gmail.com>
2016-04-09 06:24:11 +0900

    Modified vim-installer

--- bin/vim-package-install.sh
+++ bin/vim-package-install.sh
@@ -11,3 +11,8 @@ if [ ! -e $VIM_HOME/vim-hybrid ]; then
    git clone git@github.com:w0ng/vim-hybrid.git $VIM_HOME/vim-hybrid
    ln -sf $VIM_HOME/vim-hybrid/colors $VIM_HOME/colors
 fi
+
+ln -sf $HOME/.dotfiles/vimrc $HOME/.vimrc
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh ./install.sh
+rm -rf install.sh
