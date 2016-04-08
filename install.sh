c2192d292647847a3175d0205460d30e233e2dff
iory <ab.ioryz@gmail.com>
2016-04-09 03:05:24 +0900

    Modified bash install script to apply name-block-method

--- install.sh
+++ install.sh
@@ -1,94 +1,91 @@
 #!/bin/sh
 
-export DISTRIB_RELEASE=`cat /etc/lsb-release | grep DISTRIB_RELEASE | tr --delete DISTRIB_RELEASE=`
+[ ! -d ${HOME}/local ] && mkdir ${HOME}/local
+[ ! -d ${HOME}/bin ] && mkdir ${HOME}/bin
 
-if [ ! -d ${HOME}/local ]; then
-    mkdir ${HOME}/local
-fi
-if [ ! -d ${HOME}/bin ]; then
-    mkdir ${HOME}/bin
-fi
+: "install emacs 24.5" && {
+    EMACS_VERSION=24.5
+    sudo apt-get install -y build-essential
+    sudo apt-get build-dep -y emacs
+    sudo apt-get install -y libgif-dev
+    cd ${HOME}/local
+    if [ ! -d emacs-${EMACS_VERSION}.tar.xz ]; then
+        wget -O- http://ftp.gnu.org/gnu/emacs/emacs-${EMACS_VERSION}.tar.xz | tar xJf -
+    fi
+    (cd emacs-${EMACS_VERSION} \
+            && ./configure \
+            && make \
+            && sudo make install;)
+}
 
-EMACS_VERSION=24.5
-# if [ ${DISTRIB_RELEASE} = "12.04" ]; then
-#     sudo add-apt-repository -y ppa:cassou/emacs
-#     sudo apt-get update
-#     sudo apt-get install -y emacs24 emacs24-el
-# else
-#     sudo apt-get install -y emacs
-# fi
+: "install apt package" && {
+    sudo apt-get install -y cmigemo migemo
+    sudo apt-get install -y curl
+    sudo apt-get install -y emacs-mozc
+    sudo apt-get install -y global
+    sudo apt-get install -y rlwrap
+    sudo apt-get install -y source-highlight
+    sudo apt-get install -y ssh
+    sudo apt-get install -y tmux
+    sudo apt-get install -y xsel
+    sudo apt-get install -y zsh
+}
 
-# install emacs 24.5
-sudo apt-get install -y build-essential
-sudo apt-get build-dep -y emacs
-sudo apt-get install -y libgif-dev
-cd ${HOME}/local
-if [ ! -d emacs-${EMACS_VERSION}.tar.xz ]; then
-    wget -O- http://ftp.gnu.org/gnu/emacs/emacs-${EMACS_VERSION}.tar.xz | tar xJf -
-fi
-(cd emacs-${EMACS_VERSION} \
-        && ./configure \
-        && make \
-        && sudo make install;)
+: "hub command install (github)" && {
+    case ${OSTYPE} in
+        darwin*)
+            brew install hub
+            ;;
+        linux*)
+            if [ ! -d ~/bin/hub ]; then
+                curl https://hub.github.com/standalone -sLo ~/bin/hub
+            fi
+            chmod +x ~/bin/hub
+            ;;
+    esac
+}
 
-sudo apt-get install -y cmigemo migemo
-sudo apt-get install -y curl
-sudo apt-get install -y emacs-mozc
-sudo apt-get install -y global
-sudo apt-get install -y rlwrap
-sudo apt-get install -y source-highlight
-sudo apt-get install -y ssh
-sudo apt-get install -y tmux
-sudo apt-get install -y xsel
-sudo apt-get install -y zsh
+: "set zsh" && {
+    sudo chsh -s `which zsh`
+    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+}
 
-# hub command install (github)
-case ${OSTYPE} in
-      darwin*)
-          brew install hub
-          ;;
-      linux*)
-          if [ ! -d ~/bin/hub ]; then
-              curl https://hub.github.com/standalone -sLo ~/bin/hub
-          fi
-          chmod +x ~/bin/hub
-          ;;
-esac
+: "percol settings" && {
+    if [ ! -e $HOME/.percol.d ]; then
+        mkdir $HOME/.percol.d
+    fi
+    ln -sf ~/.dotfiles/rc.py ~/.percol.d/rc.py
+}
 
-# sudo chsh -s `which zsh`
-# sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+: "create symbolic links of config" && {
+    ln -sf ~/.dotfiles/.gdbinit ~/.gdbinit
+    ln -sf ~/.dotfiles/.pythonstartup ~/.pythonstartup
+    ln -sf ~/.dotfiles/.xmodmaprc ~/.xmodmaprc
+    ln -sf ~/.dotfiles/.zsh/.zshenv ~/.zshenv
+    ln -sf ~/.dotfiles/vimrc ~/.vimrc
+    ln -sf ~/.dotfiles/.agignore ~/.agignore
+    ln -sf ~/.dotfiles/.gitignore_global ~/.gitignore
+    ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+    ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+    ln -sf ~/.dotfiles/.xinitrc ~/.xinitrc
+}
 
-ln -sf ~/.dotfiles/.gdbinit ~/.gdbinit
-ln -sf ~/.dotfiles/.pythonstartup ~/.pythonstartup
-ln -sf ~/.dotfiles/.xmodmaprc ~/.xmodmaprc
-ln -sf ~/.dotfiles/.zsh/.zshenv ~/.zshenv
-ln -sf ~/.dotfiles/vimrc ~/.vimrc
+: "ipython settings" && {
+    if [ ! -e $HOME/.ipython/profile_default/startup ]; then
+        mkdir -p $HOME/.ipython/profile_default/startup
+    fi
+    ln -sf ~/.dotfiles/ipython-settings/00-first.py ~/.ipython/profile_default/startup/00-first.py
+}
 
-# ipython settings
-if [ ! -e $HOME/.ipython/profile_default/startup ]; then
-    mkdir -p $HOME/.ipython/profile_default/startup
-fi
-ln -sf ~/.dotfiles/ipython-settings/00-first.py ~/.ipython/profile_default/startup/00-first.py
+: "apply rlwrap to roseus" && {
+    ln -sf ~/.dotfiles/completions/.roseus_completions ~/.roseus_completions
+}
 
-ln -sf ~/.dotfiles/.agignore ~/.agignore
-ln -sf ~/.dotfiles/.gitignore_global ~/.gitignore
-ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/.dotfiles/.xinitrc ~/.xinitrc
-
-# percol settings
-if [ ! -e $HOME/.percol.d ]; then
-    mkdir $HOME/.percol.d
-fi
-ln -sf ~/.dotfiles/rc.py ~/.percol.d/rc.py
-
-# for lisp
-ln -sf ~/.dotfiles/completions/.roseus_completions ~/.roseus_completions
-
-# for arduino
-case ${OSTYPE} in
-    linux*)
-    sudo ln -sf ~/.dotfiles/udev-settings/90-kinect2.rules /etc/udev/rules.d/90-kinect2.rules
-    sudo ln -sf ~/.dotfiles/udev-settings/10-arduino.rules /etc/udev/rules.d/10-arduino.rules
-    sudo ln -sf ~/.dotfiles/udev-settings/11-arduino.rules /etc/udev/rules.d/11-arduino.rules
-esac
+: "udev conf for device" && {
+    case ${OSTYPE} in
+        linux*)
+            sudo ln -sf ~/.dotfiles/udev-settings/90-kinect2.rules /etc/udev/rules.d/90-kinect2.rules
+            sudo ln -sf ~/.dotfiles/udev-settings/10-arduino.rules /etc/udev/rules.d/10-arduino.rules
+            sudo ln -sf ~/.dotfiles/udev-settings/11-arduino.rules /etc/udev/rules.d/11-arduino.rules
+    esac
+}
