da8992b9d9f642bb4cc53c6b7b43dc7c8c6990d2
iory <ab.ioryz@gmail.com>
2016-06-04 18:56:29 +0900

    Modified disabling launcher in ubuntu

--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -2,3 +2,6 @@
 
 # workspace like a Mac
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 4
+
+# disable pop up of dash by super key
+dconf write /org/compiz/profiles/unity/plugins/unityshell/show-launcher '""'
