3f0ff05ca76598ce4f058040c662c881465e4305
iory <ab.ioryz@gmail.com>
2016-11-14 17:29:37 +0900

    [emacs.sh] Enabled emacsclient in terminal

--- sh/plugins/emacs.sh
+++ sh/plugins/emacs.sh
@@ -3,11 +3,10 @@
 # setting default editor
 alias emacs='emacs-snapshot'
 export EDITOR="emacs"
-if [[ ${DISPLAY} == ":0" ]] ; then
-    export EDITOR="emacsclient"
-    alias emacs-shell='emacs -nw -q -f shell'
-    function emacsclient_ () {
-        emacsclient -a "" $1 &
-    }
-    alias e='emacsclient_'
-fi
+
+export EDITOR="emacsclient"
+alias emacs-shell='emacs -nw -q -f shell'
+function emacsclient_ () {
+    emacsclient -a "" $1 &
+}
+alias e='emacsclient_'
