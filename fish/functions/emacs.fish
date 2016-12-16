473fcb3babc78edfcfe6514aa3322d0a5ea993f0
iory <ab.ioryz@gmail.com>
2016-12-16 15:47:41 +0900

    [fish] Modified emacsclient

--- fish/functions/emacs.fish
+++ fish/functions/emacs.fish
@@ -5,4 +5,7 @@ alias emacs 'emacs-snapshot'
 set -x EDITOR "emacs"
 set -x EDITOR "emacsclient"
 alias emacs-shell 'emacs -nw -q -f shell'
-alias e 'emacsclient -a ""'
+function emacsclient_
+    emacsclient -a "" $argv[1] &
+end
+alias e 'emacsclient_'
