66d0c71a8ae62cd868fb446965b7dd7539da31ca
iory <ab.ioryz@gmail.com>
2017-01-04 19:55:32 +0900

    [scripts] Modified check_ssh checking config file

--- scripts/check_ssh.py
+++ scripts/check_ssh.py
@@ -16,7 +16,8 @@ if not os.path.exists(os.path.expanduser("~/.ssh")):
 path_permission = [('~/', '755'),
                    ('~/.ssh', '700'),
                    ('~/.ssh/authorized_keys', '600'),
-                   ('~/.ssh/id_rsa', '600')
+                   ('~/.ssh/id_rsa', '600'),
+                   ('~/.ssh/config', '600'),
 ]
 
 print(Fore.CYAN, '[check ssh server and client settings]')
