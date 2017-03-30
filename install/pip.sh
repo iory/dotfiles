01583089542cc5cd938b8fb1f5edeb525df82879
iory <ab.ioryz@gmail.com>
2017-03-30 14:26:23 +0900

    [install] Add pip installer

---
+++ install/pip.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -eu
+
+pip install -U -q --upgrade pip
+pip install -U ipython
+
+pip install -U autopep8
+pip install -U dpkt
+pip install -U epc
+pip install -U grip
+pip install -U jedi
+pip install -U mahotas
+pip install -U matplotlib
+pip install -U numpy
+pip install -U pandas
+pip install -U percol
+pip install -U pillow
+pip install -U pyflakes
+pip install -U pygeoip
+pip install -U pyperclip
+pip install -U requests
+pip install -U scikit-image
+pip install -U scikit-learn
+pip install -U scipy
+pip install -U see
+pip install -U virtualenv
+pip install autopep8
+pip install flake8
+pip install importmagic
+pip install jedi
+pip install rope
+pip install yapf
