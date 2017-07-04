3b24bda6c84bcdf141cf7d08a8ce441e12aac898
iory <ab.ioryz@gmail.com>
2017-07-05 05:21:39 +0900

    [fish] Modified recented

--- fish/config.fish
+++ fish/config.fish
@@ -18,7 +18,7 @@ end
 function fish_user_key_bindings
     bind \cr 'peco_select_history (commandline -b)'
     bind \cxk peco_kill
-    bind \cx\cr peco_recentd
+    bind \cc\cr peco_recentd
 end
 
 
