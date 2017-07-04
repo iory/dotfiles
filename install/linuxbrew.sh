e416989ded273278e08b028a3adcceb898c71486
iory <ab.ioryz@gmail.com>
2017-07-04 18:44:35 +0900

    [fish] Add linuxbrew

--- install/linuxbrew.sh
+++ install/linuxbrew.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -eu
 
-git clone https://github.com/Homebrew/linuxbrew.git ~/.linuxbrew
+if [ ! -d $HOME/.linuxbrew ]; then
+    git clone https://github.com/Linuxbrew/brew.git $HOME/.linuxbrew
+else
+    (cd $HOME/.linuxbrew && git pull)
+fi
 
 echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' >> ~/.zshrc.local
 echo 'export LD_LIBRARY_PATH="$HOME/.linuxbrew/lib:$LD_LIBRARY_PATH"' >> ~/.zshrc.local
 
 echo 'set -gx PATH $HOME/.linuxbrew/bin $PATH' >> ~/.fishrc.local
 echo 'set -gx LD_LIBRARY_PATH $HOME/.linuxbrew/lib $LD_LIBRARY_PATH' >> ~/.fishrc.local
+echo 'set -x MANPATH $HOME/.linuxbrew/share/man $MANPATH' >> ~/.fishrc.local
+echo 'set -x INFOPATH $HOME/.linuxbrew/share/info $INFOPATH' >> ~/.fishrc.local
