b39f017b149e196b26bfb979b15c2a051a884974
iory <ab.ioryz@gmail.com>
2017-07-04 18:10:50 +0900

    [scrips] Add install cuda smi

---
+++ scripts/install_cuda_smi.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if ! which nvcc &>/dev/null; then
+  exit 0
+fi
+
+if which cuda-smi &>/dev/null; then
+  exit 0
+fi
+
+TMPDIR=""
+
+if [ "$(uname)" = "Linux" ]; then
+  TMPDIR=$(mktemp -d)
+elif [ "$(uname)" = "Darwin" ]; then
+  TMPDIR=$(mktemp -d /tmp/tmp.XXXX)
+else
+  echo "[install_cuda_smi.bash] Unsupported arch: $(uname)"
+  exit 1
+fi
+
+cd $TMPDIR
+
+git clone https://github.com/al42and/cuda-smi
+cd cuda-smi
+
+make
+mkdir -p $HOME/.local/bin
+cp $(pwd)/cuda-smi $HOME/.local/bin/cuda-smi
+
+rm -rf $TMPDIR
