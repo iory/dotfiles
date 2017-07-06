0964cd1e2d31125f5d8da79f2c5f049c16d0d9a4
iory <ab.ioryz@gmail.com>
2017-07-07 03:14:39 +0900

    [fish] Add login install

--- fish/install.fish
+++ fish/install.fish
@@ -3,7 +3,25 @@
 set -l DIR (functions -e cd (dirname (status -f)); and pwd)
 cd $DIR
 
+function options
+    echo $argv | sed 's|--*|\\'\n'|g' | grep -v '^$'
+end
+
+function function_with_options
+    for i in (options $argv)
+        echo $i | read -l option value
+
+        switch $option
+            case login l
+                echo $DIR/.profile "->" $HOME/.profile
+                ln -sf $DIR/.profile $HOME/.profile
+        end
+    end
+end
+
 echo "installing fish files ..."
+function_with_options $argv
+
 for file in (git ls-files)
     switch $file
         case "*.fish"
