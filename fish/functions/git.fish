249ae80fee78477fe511a227a0d523f9eb6e9f41
iory <ab.ioryz@gmail.com>
2017-07-12 01:19:27 +0900

    [fish] Modified alias -> abbr

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -5,78 +5,78 @@
 ###############
 
 # basic command
-alias grh!='git reset --hard'
-alias grh~='git reset HEAD~'
-alias grh~1='git reset HEAD~1'
-alias grh~2='git reset HEAD~2'
-alias grh~3='git reset HEAD~3'
-alias g1msg='git log -1 --format="%s" | cat'
-alias g1sh='git log -1 --format="%h" | cat'
-alias gram='git remote add {$GITHUB_USER}; and git fetch {$GITHUB_USER}'
+abbr -a grh! 'git reset --hard'
+abbr -a grh~ 'git reset HEAD~'
+abbr -a grh~1 'git reset HEAD~1'
+abbr -a grh~2 'git reset HEAD~2'
+abbr -a grh~3 'git reset HEAD~3'
+abbr -a g1msg 'git log -1 --format="%s" | cat'
+abbr -a g1sh 'git log -1 --format="%h" | cat'
+abbr -a gram 'git remote add {$GITHUB_USER}; and git fetch {$GITHUB_USER}'
 
 # Use hub as git client
-# type hub &>/dev/null && alias git=hub
+# type hub &>/dev/null && abbr -a git=hub
 
-alias ga='git add'
-alias ga.='git add .'
-alias gap='git add -p'
-alias gb='git branch'
-alias gbD='git branch -D'
-alias gba='git branch -a'
-alias gcm='git commit -m'
-alias gca="git commit --amend"
-alias gco='git checkout'
-alias gcop='git checkout -p --'
-alias gcob='git checkout -b'
-alias gcom='git checkout master'
-alias gl='git log --oneline'
-alias gln="git log --name-only"
-alias glp="git log -p"
-alias glg='git log --graph --oneline --decorate --all'
-alias gld='git log --pretty=format:"%h %ad %s" --date=short --all'
+abbr -a ga 'git add'
+abbr -a ga. 'git add .'
+abbr -a gap 'git add -p'
+abbr -a gb 'git branch'
+abbr -a gbD 'git branch -D'
+abbr -a gba 'git branch -a'
+abbr -a gcm 'git commit -m'
+abbr -a gca="git commit --amend"
+abbr -a gco 'git checkout'
+abbr -a gcop 'git checkout -p --'
+abbr -a gcob 'git checkout -b'
+abbr -a gcom 'git checkout master'
+abbr -a gl 'git log --oneline'
+abbr -a gln 'git log --name-only'
+abbr -a glp 'git log -p'
+abbr -a glg 'git log --graph --oneline --decorate --all'
+abbr -a gld 'git log --pretty=format:"%h %ad %s" --date=short --all'
 
 function _git_current_branch
     git branch ^/dev/null | sed -n '/\* /s///p'
 end
 
-alias gbug='git branch -u origin/(_git_current_branch)'
-alias gbum='git branch -u $GITHUB_USER/(_git_current_branch)'
-alias gml='git pull $GITHUB_USER (_git_current_branch)'
-alias gmpull='git pull $GITHUB_USER (_git_current_branch)'
-alias gmpnp='git pull --rebase $GITHUB_USER (_git_current_branch); and git push $GITHUB_USER (_git_current_branch)'
-alias gmpnpf='git pull --rebase $GITHUB_USER (_git_current_branch); and git push $GITHUB_USER (_git_current_branch) -f'
+abbr -a gbug 'git branch -u origin/(_git_current_branch)'
+abbr -a gbum 'git branch -u $GITHUB_USER/(_git_current_branch)'
+abbr -a gml 'git pull $GITHUB_USER (_git_current_branch)'
+abbr -a gmpull 'git pull $GITHUB_USER (_git_current_branch)'
+abbr -a gmpnp 'git pull --rebase $GITHUB_USER (_git_current_branch); and git push $GITHUB_USER (_git_current_branch)'
+abbr -a gmpnpf 'git pull --rebase $GITHUB_USER (_git_current_branch); and git push $GITHUB_USER (_git_current_branch) -f'
 
-alias gbm='git branch --all | command grep $GITHUB_USER | sed "s/ *//g"'
+abbr -a gbm 'git branch --all | command grep $GITHUB_USER | sed "s/ *//g"'
 
-alias gd='git diff'
-alias gdw='git diff -w'
-alias gdn='git diff --name-only'
-alias gds='git diff --staged'
-alias gda='git diff HEAD'
+abbr -a gd 'git diff'
+abbr -a gdw 'git diff -w'
+abbr -a gdn 'git diff --name-only'
+abbr -a gds 'git diff --staged'
+abbr -a gda 'git diff HEAD'
 # For example, gd5=git diff HEAD~5
 for n in (seq 50)
-    alias gd$n="git diff HEAD~$n"
-    alias gdn$n="git diff --name-only HEAD~$n"
+    abbr -a gd$n 'git diff HEAD~$n'
+    abbr -a gdn$n 'git diff --name-only HEAD~$n'
 end
-alias first-commit='git init; and git commit --allow-empty -m "First commit"'
+abbr -a first-commit 'git init; and git commit --allow-empty -m "First commit"'
 
-alias gs='git status'
-alias gst='git stash'
-alias gstp='git stash pop'
-alias gstl='git stash list'
-alias gstd='git stash drop'
+abbr -a gs 'git status'
+abbr -a gst 'git stash'
+abbr -a gstp 'git stash pop'
+abbr -a gstl 'git stash list'
+abbr -a gstd 'git stash drop'
 
-alias gr='git remote -v'
-alias grv='git remote -v'
+abbr -a gr 'git remote -v'
+abbr -a grv 'git remote -v'
 
-alias gans="git diff -w --no-color | git apply --cached --ignore-whitespace"
+abbr -a gans="git diff -w --no-color | git apply --cached --ignore-whitespace"
 
 ###############
 # Git Funcion #
 ###############
 
 function git-branch-update
-    set -l branch (git rev-parse --abbrev-ref HEAD)
+    set -l branch (git rev-parse --abbr -aev-ref HEAD)
     git fetch --all
     switch (count $argv)
         case 0
@@ -85,23 +85,23 @@ function git-branch-update
             git reset --hard $argv[1]/$branch
     end
 end
-alias gpu=git-branch-update
+abbr -a gpu git-branch-update
 
 function git-pull
     switch (count $argv)
         case 0
             set repository origin
-            set branch (git rev-parse --abbrev-ref HEAD)
+            set branch (git rev-parse --abbr -aev-ref HEAD)
         case 1
             set repository $argv[1]
-            set branch (git rev-parse --abbrev-ref HEAD)
+            set branch (git rev-parse --abbr -aev-ref HEAD)
         case '*'
             set repository $argv[1]
             set branch $argv[2]
     end
     git pull $repository $branch
 end
-alias gps=git-pull
+abbr -a gps git-pull
 
 # # Git log find by commit message
 function glf
@@ -118,7 +118,7 @@ function gis
             hub browse $argv[1] issues/$argv[2] >/dev/null 2>&1
     end
 end
-alias gbw='hub browse ^ /dev/null'
+abbr -a gbw 'hub browse ^ /dev/null'
 
 hub alias -s  >/dev/null 2>&1
-alias git='hub'
+abbr -a git 'hub'
