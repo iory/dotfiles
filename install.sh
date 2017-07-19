4263e7adbe490294973c552bd8e8220f7f810708
iory <ab.ioryz@gmail.com>
2017-07-20 06:56:43 +0900

    [vim] Refactor

--- install.sh
+++ install.sh
@@ -24,21 +24,6 @@ current_working_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 }
 
 : "vim install" && {
-    VIM_HOME=$HOME/.vim
-
-    [ ! -d $VIM_HOME ] && mkdir $VIM_HOME
-
-    # if [ ! -e $VIM_HOME/nerdtree ]
-    # git clone https://github.com/scrooloose/nerdtree.git ~/.vim
-
-    if [ ! -e $VIM_HOME/vim-hybrid ]; then
-        git clone http://github.com/w0ng/vim-hybrid.git $VIM_HOME/vim-hybrid
-        ln -sf $VIM_HOME/vim-hybrid/colors $VIM_HOME/colors
-    fi
-
-    ln -sf `pwd`/.vimrc $HOME/.vimrc
-    curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | bash
-
     mkdir -p $HOME/.config/nvim
     ln -sf `pwd`/nvim/dein $HOME/.config/dein
     ln -sf `pwd`/nvim/init.vim $HOME/.config/nvim/init.vim
