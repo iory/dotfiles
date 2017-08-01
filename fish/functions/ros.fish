2c22578b60cd43af456a118d2074f1d7a0d99591
iory <ab.ioryz@gmail.com>
2017-08-02 04:42:45 +0900

    [fish/ros] Refactor

--- fish/functions/ros.fish
+++ fish/functions/ros.fish
@@ -4,54 +4,55 @@
 # ROS #
 #######
 
-if test -d "/opt/ros"
-    alias re="rostopic echo"
-    alias ri="rostopic info"
-    alias rt="rostopic"
+if test -d '/opt/ros'
+    source ~/.dotfiles/rosfish
+    abbr -a re 'rostopic echo'
+    abbr -a ri 'rostopic info'
+    abbr -a rt 'rostopic'
 
-    alias rc='roscd'
-    alias rr='rosrun'
-    alias rl='roslaunch'
+    abbr -a rc 'roscd'
+    abbr -a rr 'rosrun'
+    abbr -a rl 'roslaunch'
     # rostopic
-    alias rp='rostopic'
-    alias rpb='rostopic bw'
-    alias rpe='rostopic echo'
-    alias rpt='rostopic type'
-    alias rph='rostopic hz'
-    alias rpi='rostopic info'
+    abbr -a rp 'rostopic'
+    abbr -a rpb 'rostopic bw'
+    abbr -a rpe 'rostopic echo'
+    abbr -a rpt 'rostopic type'
+    abbr -a rph 'rostopic hz'
+    abbr -a rpi 'rostopic info'
     function ros_topic_info_detail
         echo '#################
 # rostopic info #
 #################
         '
-        rostopic info $1
+        rostopic info $argv[1]
         echo '#################
 # rostopic type #
 #################
         '
-        rostopic type $1
+        rostopic type $argv[1]
         echo '\n###############
 # rosmsg show #
 ###############
         '
-        rostopic type $1 | rosmsg show
+        rostopic type $argv[1] | rosmsg show
     end
 
-    alias rpid='ros_topic_info_detail'
-    alias rpl='rostopic list'
+    abbr -a rpid 'ros_topic_info_detail'
+    abbr -a rpl 'rostopic list'
     # rosnode
-    alias rn='rosnode'
-    alias rni='rosnode info'
-    alias rnl='rosnode list'
-    alias rnk='rosnode kill'
+    abbr -a rn 'rosnode'
+    abbr -a rni 'rosnode info'
+    abbr -a rnl 'rosnode list'
+    abbr -a rnk 'rosnode kill'
     # rosservice
-    alias rs='rosservice'
-    alias rsl='rosservice list'
+    abbr -a rs 'rosservice'
+    abbr -a rsl 'rosservice list'
     # rqt_plugins
-    alias rqt_gui='rosrun rqt_gui rqt_gui'
-    alias rqt_reconfigure='rosrun rqt_reconfigure rqt_reconfigure'
-    alias rqt_image_view='rosrun rqt_image_view rqt_image_view'
-    alias republish='rosrun image_transport republish'
+    abbr -a rqt_gui 'rosrun rqt_gui rqt_gui'
+    abbr -a rqt_reconfigure 'rosrun rqt_reconfigure rqt_reconfigure'
+    abbr -a rqt_image_view 'rosrun rqt_image_view rqt_image_view'
+    abbr -a republish 'rosrun image_transport republish'
     function image_publisher
         rosrun jsk_perception image_publisher.py _file_name:=$argv[1]
     end
@@ -65,95 +66,61 @@ if test -d "/opt/ros"
         rosrun image_view2 image_view2 image:=$argv[1]
     end
 
-    # _image_topics () {
-    # local -a reply
-    # if [[ ${CURRENT} = 2 ]]; then
-    #     reply=($(rostopic find sensor_msgs/Image))
-    #     fi
-    #     _describe 'values' reply
-    #     }
-#     if which compdef &>/dev/null; then
-#         compdef _image_topics image_view
-#         compdef _image_topics image_view2
-#         fi
-    alias imv='image_view'
-    alias imv2='image_view2'
+    abbr -a imv 'image_view'
+    abbr -a imv2 'image_view2'
     # nodelet
-    alias nodelet_standalone='rosrun nodelet nodelet standalone'
+    abbr -a nodelet_standalone 'rosrun nodelet nodelet standalone'
 
-    alias tf='cd /tmp and rosrun tf view_frames and evince frames.pdf'
-    alias tf_view='rosrun tf view_frames'
+    abbr -a tf 'cd /tmp and rosrun tf view_frames and evince frames.pdf'
+    abbr -a tf_view 'rosrun tf view_frames'
 end
 
 # wstool
-alias ws=wstool
-alias wsi='wstool info -s'
-alias wscd='wstool_cd'
-alias wsset='wstool set'
-alias wsup='wstool update'
-alias wsrm='wstool remove'
-alias wslist='wstool info --only=localname'
+abbr -a ws wstool
+abbr -a wsi 'wstool info -s'
+abbr -a wscd 'wstool_cd'
+abbr -a wsset 'wstool set'
+abbr -a wsup 'wstool update'
+abbr -a wsrm 'wstool remove'
+abbr -a wslist 'wstool info --only=localname'
 function wssethub
     wstool set $argv[1] https://github.com/$argv[1].git --git
 end
-#         wsg () {
-#         if [ "$1" = "" ]; then
-#             exit 1
-#             fi
-#             USER_REPO=$(echo "$1" | sed -e 's/https:\/\/github.com\///g' | sed -e 's/git@github.com\///g')
-#             USER=$(echo $USER_REPO | sed -e "s/\// /g" | awk '{print $1}')
-#             REPOSITORY=$(echo $USER_REPO | sed -e "s/\// /g" | awk '{print $2}')
-#             if [ "$REPOSITORY" = "" ]; then
-#                 REPOSITORY="$USER"
-#                 wstool set $REPOSITORY https://github.com/$REPOSITORY --git -y -u
-#             else
-#                 wstool set $REPOSITORY https://github.com/$USER/$REPOSITORY --git -y -u
-#                 fi
-#                 if [ $TMP -eq 1 ]; then
-#                     echo "Removing"
-#                     wstool remove $REPOSITORY
-#                     elif [ $TMP -eq 2 ]; then
-#                     echo "Update"
-#                     wstool up $REPOSITORY
-#                     fi
-#                     }
 
 # catkin-tools
-alias c='catkin'
-alias cb='catkin build'
-alias cbt='catkin bt'
-alias cbts='catkin bt --start-with-this'
-alias cbtsf='catkin bt --start-with-this --force-cmake'
-alias cbtv='catkin bt --verbose'
-alias crt='catkin run_tests'
-alias crtt='catkin run_tests --this --no-deps -iv'
-alias crl='catkin roslint'
-alias crlt='catkin roslint --this --no-deps -iv'
-alias cli='catkin lint'
+abbr -a c 'catkin'
+abbr -a cb 'catkin build'
+abbr -a cbt 'catkin bt'
+abbr -a cbts 'catkin bt --start-with-this'
+abbr -a cbtsf 'catkin bt --start-with-this --force-cmake'
+abbr -a cbtv 'catkin bt --verbose'
+abbr -a crt 'catkin run_tests'
+abbr -a crtt 'catkin run_tests --this --no-deps -iv'
+abbr -a crl 'catkin roslint'
+abbr -a crlt 'catkin roslint --this --no-deps -iv'
+abbr -a cli 'catkin lint'
 
 function imv
     rosrun image_view image_view image:=$argv[1]
 end
 
-if [ "$EMACS" ]; then
-    :
+if test -n '$EMACS'
 else
-    alias roseus="rlwrap -c -b '(){}.,;|' -a -pGREEN roseus"
-    alias irteus='rlwrap -c irteus'
-    alias irteusgl='rlwrap -c irteusgl'
+    abbr -a roseus 'rlwrap -c -b "(){}.,;|" -a -pGREEN roseus'
+    abbr -a irteus 'rlwrap -c irteus'
+    abbr -a irteusgl 'rlwrap -c irteusgl'
 end
 
 switch (uname)
     case Linux
-        if test -e "/opt/ros"
+        if test -d '/opt/ros'
             # ros setting
             switch (lsb_release -sr)
-                case "14.04"
+                case '14.04'
                     bash /opt/ros/indigo/setup.bash
-                case "12.04"
+                case '12.04'
                     bash /opt/ros/hydro/setup.bash
             end
         end
     case Darwin
-        :
 end
