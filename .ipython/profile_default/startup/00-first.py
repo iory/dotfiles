571ff2f37580d6d8424169eee12de963d55a827b
iory <ab.ioryz@gmail.com>
2017-09-06 18:01:57 +0900

    [ipython] Modified default import library

--- .ipython/profile_default/startup/00-first.py
+++ .ipython/profile_default/startup/00-first.py
@@ -1,58 +1,19 @@
-from decimal import Decimal as D
-import matplotlib as mpl
-mpl.use('Agg')
-import matplotlib.pyplot as plt
-import array
-import bisect
+try:
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+except ImportError:
+    pass
+
 import collections
-import decimal
-import fractions
-import heapq
 import itertools
-import math
-import operator as op
 import os
 import os.path as osp
-import pprint
-import random
-import re
-import string
 import sys
-import time
+import datetime
 
-#import reduce
-from functools import reduce
-
-from see import see
-
-import numpy
-import numpy as np
-import cv2
-import scipy
-import scipy as sp
-import pandas as pd
-import seaborn as sns
-
-import chainer
-import chainer.functions as F
-import chainer.links as L
-
-
-def mem(obj):
-    return hex(id(obj))
-
-
-def cvshow(img):
-    if img.ndim == 2:  # gray image
-        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    plt.show()
-
-
-def imshow(img):
-    plt.imshow(img)
-    plt.show()
-
-
-print("type()    -> analyze type of objects.")
-print("see()     -> extend dir().")
+try:
+    import numpy
+    import numpy as np
+except ImportError:
+    pass
