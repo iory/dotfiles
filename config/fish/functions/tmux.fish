4913a6a4d8ef8e4e785784ec6d79c3b6bb75180b
iory <ab.ioryz@gmail.com>
2017-10-24 06:28:49 +0900

    [fish/tmux] Modified

--- config/fish/functions/tmux.fish
+++ config/fish/functions/tmux.fish
@@ -1,5 +1,5 @@
 function ft -d "Switch tmux session"
-    set LIST_SESSIONS (tmux list-sessions)
+    set LIST_SESSIONS (tmux list-sessions 2> /dev/null)
     if test $status -eq 0
         set N_LINE (tmux list-sessions | wc | awk '{print $1}')
         if test "$N_LINE" -eq 1
@@ -18,5 +18,7 @@ function ft -d "Switch tmux session"
                 tmux list-sessions | fzf | awk '{print $1}' | tr -d ":" | xargs tmux switch-client -t
             end
         end
+    else
+        tmux
     end
 end
