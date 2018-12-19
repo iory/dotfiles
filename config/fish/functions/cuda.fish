afde24082429f971e3c404bb5c525c4180f351ba
iory <ab.ioryz@gmail.com>
2018-12-19 21:42:47 +0900

    [fish] Add cuda path if /usr/local/cuda path exists

--- config/fish/functions/cuda.fish
+++ config/fish/functions/cuda.fish
@@ -1,7 +1,9 @@
 #!/bin/fish
 
-set -x CUDA_PATH /usr/local/cuda
-set -x PATH $CUDA_PATH/bin $PATH
-set -x LD_LIBRARY_PATH $CUDA_PATH/lib64:$LD_LIBRARY_PATH
-set -x CPATH $CUDA_PATH/include:$CPATH
-set -x LIBRARY_PATH $CUDA_PATH/lib64:$LD_LIBRARY_PATH
+if test -z "/usr/local/cuda"
+    set -x CUDA_PATH /usr/local/cuda
+    set -x PATH $CUDA_PATH/bin $PATH
+    set -x LD_LIBRARY_PATH $CUDA_PATH/lib64:$LD_LIBRARY_PATH
+    set -x CPATH $CUDA_PATH/include:$CPATH
+    set -x LIBRARY_PATH $CUDA_PATH/lib64:$LD_LIBRARY_PATH
+end
