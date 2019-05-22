8f5b06832198de84caf3b9ba45b39340e37d5fb0
iory <ab.ioryz@gmail.com>
2019-05-23 02:56:18 +0900

    [config/fish/tmux.fish] Modified capture alias

--- config/fish/functions/tmux.fish
+++ config/fish/functions/tmux.fish
@@ -39,8 +39,8 @@ function tmux-capture-pane-edit -d "capture pane and open by $EDITOR"
     eval $EDITOR ~/.tmux/log/$DIR/$FILENAME.txt
 end
 
-abbr -a ts 'tmux-capture-pane'
-abbr -a tse 'e (tmux-capture-pane)'
+alias ts='tmux-capture-pane'
+alias tse='$EDITOR (tmux-capture-pane)'
 
 alias tp="tmux capture-pane -S -1000000000 -e && tmux save-buffer - | less -r +Gk"
 alias tv="tmux capture-pane -S -1000000000 && tmux save-buffer - | vim - +'set nonumber' +'norm G' -R"
