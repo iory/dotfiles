0fd2212e00ec455d20b47796cd19d42e5623e738
iory <ab.ioryz@gmail.com>
2019-05-25 01:25:46 +0900

    [config/fish/python.fish] Update abbr

--- config/fish/functions/python.fish
+++ config/fish/functions/python.fish
@@ -1,11 +1,11 @@
 # python settings
-abbr -a p 'ipython --no-confirm-exit'
-abbr -a py 'ipython --no-confirm-exit'
-abbr -a ipy 'ipython --no-confirm-exit'
-abbr -a ipyt 'ipython --no-confirm-exit'
-abbr -a ipyth 'ipython --no-confirm-exit'
-abbr -a ipytho 'ipython --no-confirm-exit'
-abbr -a ipython 'ipython --no-confirm-exit'
+abbr -a p 'ipython --no-confirm-exit --'
+abbr -a py 'ipython --no-confirm-exit --'
+abbr -a ipy 'ipython --no-confirm-exit --'
+abbr -a ipyt 'ipython --no-confirm-exit --'
+abbr -a ipyth 'ipython --no-confirm-exit --'
+abbr -a ipytho 'ipython --no-confirm-exit --'
+abbr -a ipython 'ipython --no-confirm-exit --'
 
 # for opencv
 abbr -a fcv=h 'python -c "import cv2; print(\"\n\".join([item for item in dir(cv2)]))" | grep'
