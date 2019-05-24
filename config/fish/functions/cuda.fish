22a24883a89673af7260526f528afb2a55db739e
iory <ab.ioryz@gmail.com>
2019-05-25 02:38:40 +0900

    [config/fish/cuda.fish] Add cuda-smi wrapper

--- config/fish/functions/cuda.fish
+++ config/fish/functions/cuda.fish
@@ -11,3 +11,23 @@ if test -d "/usr/local/cuda"
     set -x CPATH "$CUDA_PATH/include":"$CPATH"
     set -x LIBRARY_PATH "$CUDA_PATH/lib64":"$LD_LIBRARY_PATH"
 end
+
+
+function cuda-smi --wraps=cuda-smi
+    command -sq cuda-smi
+    if test $status -ne 0
+        command -sq nvcc
+        if test $status -eq 0
+            set TMPDIR (mktemp -d)
+            pushd $TMPDIR
+            git clone https://github.com/al42and/cuda-smi
+            cd cuda-smi
+            make
+            mkdir -p $HOME/.local/bin
+            cp (pwd)/cuda-smi $HOME/.local/bin/cuda-smi
+            popd
+            rm -rf $TMPDIR
+        end
+    end
+    command cuda-smi
+end
