d0530f5fbae9731ddb7ccef438e7dc51df69e73a
iory <ab.ioryz@gmail.com>
2019-05-24 21:09:04 +0900

    [config/fish] Fixed cuda path

--- config/fish/functions/cuda.fish
+++ config/fish/functions/cuda.fish
@@ -1,7 +1,11 @@
 #!/bin/fish
 
-if test -z "/usr/local/cuda"
-    set -x CUDA_PATH /usr/local/cuda
+if test -d "/usr/local/cuda"
+    set -q CUDA_VERSION; or set CUDA_VERSION ""
+    if test (count $CUDA_VERSION) -gt 0
+        set -x CUDA_VERSION "-$CUDA_VERSION"
+    end
+    set -x CUDA_PATH /usr/local/cuda"$CUDA_VERSION"
     set -x PATH $CUDA_PATH/bin $PATH
     set -x LD_LIBRARY_PATH $CUDA_PATH/lib64:$LD_LIBRARY_PATH
     set -x CPATH $CUDA_PATH/include:$CPATH
