b06bc73d2d4be8977d4f255b833fc7bf443872ef
iory <ab.ioryz@gmail.com>
2019-05-24 22:03:59 +0900

    [config/fish] Add automatically check anaconda

--- config/fish/functions/anaconda-extend.fish
+++ config/fish/functions/anaconda-extend.fish
@@ -1,6 +1,17 @@
 set -q ANACONDA_VERSION; or set ANACONDA_VERSION anaconda3-5.3.1
 
 
+function check_and_update_anaconda_env
+    if test -d $HOME/.pyenv/versions/$ANACONDA_VERSION
+    else
+        set -gx PATH $PATH $HOME/.pyenv/bin
+        cd $HOME/.pyenv
+        git pull
+        pyenv install $ANACONDA_VERSION
+    end
+end
+
+
 function activate_conda_env -d 'activate function for anaconda'
     set -gx PATH $PATH $HOME/.pyenv/bin $HOME/.pyenv/versions/$ANACONDA_VERSION/bin
     if not functions -q conda
@@ -12,6 +23,6 @@ function activate_conda_env -d 'activate function for anaconda'
     end
 end
 
-
+check_and_update_anaconda_env
 alias activate 'set -gx PATH $PATH $HOME/.pyenv/versions/$ANACONDA_VERSION/bin; source (conda info --root)/etc/fish/conf.d/conda.fish'
 alias ac 'activate_conda_env'
