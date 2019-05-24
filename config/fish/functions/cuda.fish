498705f42b038dd0bd76953e3abd7d7b7b247a9c
iory <ab.ioryz@gmail.com>
2019-05-24 23:48:22 +0900

    [config/fish/cuda.fish] Fixed

--- config/fish/functions/cuda.fish
+++ config/fish/functions/cuda.fish
@@ -2,12 +2,12 @@
 
 if test -d "/usr/local/cuda"
     set -q CUDA_VERSION; or set CUDA_VERSION ""
-    if test (count $CUDA_VERSION) -gt 0
+    if test (string length $CUDA_VERSION) -gt 0
         set -x CUDA_VERSION "-$CUDA_VERSION"
     end
     set -x CUDA_PATH /usr/local/cuda"$CUDA_VERSION"
     set -x PATH $CUDA_PATH/bin $PATH
-    set -x LD_LIBRARY_PATH $CUDA_PATH/lib64:$LD_LIBRARY_PATH
-    set -x CPATH $CUDA_PATH/include:$CPATH
-    set -x LIBRARY_PATH $CUDA_PATH/lib64:$LD_LIBRARY_PATH
+    set -x LD_LIBRARY_PATH "$CUDA_PATH/lib64":"$LD_LIBRARY_PATH"
+    set -x CPATH "$CUDA_PATH/include":"$CPATH"
+    set -x LIBRARY_PATH "$CUDA_PATH/lib64":"$LD_LIBRARY_PATH"
 end
