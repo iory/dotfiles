437723b06092ec16e47ecf51a79e644a572523a1
iory <ab.ioryz@gmail.com>
2019-05-24 21:09:26 +0900

    [config/fish] Fixed python alias

--- config/fish/functions/python.fish
+++ config/fish/functions/python.fish
@@ -1,16 +1,16 @@
 set -x PYTHONSTARTUP ~/.pythonstartup
 
 # python settings
-alias p='ipython --no-confirm-exit'
-alias py='ipython --no-confirm-exit'
-alias ipy='ipython --no-confirm-exit'
-alias ipyt='ipython --no-confirm-exit'
-alias ipyth='ipython --no-confirm-exit'
-alias ipytho='ipython --no-confirm-exit'
-alias ipython='ipython --no-confirm-exit'
+abbr -a p 'ipython --no-confirm-exit'
+abbr -a py 'ipython --no-confirm-exit'
+abbr -a ipy 'ipython --no-confirm-exit'
+abbr -a ipyt 'ipython --no-confirm-exit'
+abbr -a ipyth 'ipython --no-confirm-exit'
+abbr -a ipytho 'ipython --no-confirm-exit'
+abbr -a ipython 'ipython --no-confirm-exit'
 
 # for opencv
-alias fcv='python -c "import cv2; print(\"\n\".join([item for item in dir(cv2)]))" | grep'
+abbr -a fcv=h 'python -c "import cv2; print(\"\n\".join([item for item in dir(cv2)]))" | grep'
 
 function jupyte2python -d "Convert jupyer nodebook file to python text file"
     jupyter nbconvert --to python $argv[1]
