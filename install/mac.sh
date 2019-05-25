38fde63ee8736dd8eb4d36b7359ac4b98482b9e3
iory <ab.ioryz@gmail.com>
2019-05-25 22:53:23 +0900

    [install] Update Mac settings.

--- install/mac.sh
+++ install/mac.sh
@@ -4,3 +4,6 @@ defaults write com.apple.dock mru-spaces -bool false
 
 # Disable .DS_Store
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
+# Disable crash report
+defaults write com.apple.CrashReporter DialogType -string "none"
