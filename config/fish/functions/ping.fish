7dfe3d92b403ba9fec09807ba0e26dc099a25baf
iory <ab.ioryz@gmail.com>
2019-06-01 21:28:42 +0900

    [config/fish/ping.fish] Add timestamped fish

--- config/fish/functions/ping.fish
+++ config/fish/functions/ping.fish
@@ -1,3 +1,18 @@
 function ping -d "Ping with colorized output"
     _colorize ping $argv
 end
+
+
+function ping-with-timestamp -d "Timestamped ping"
+    switch (count $argv)
+        case 0
+            bash -c 'ping $0 | while read pi; do echo "$(date \'+[%Y/%m/%d %H:%M:%S]\') $pi"; done' 8.8.8.8
+        case '*'
+            bash -c 'ping $0 | while read pi; do echo "$(date \'+[%Y/%m/%d %H:%M:%S]\') $pi"; done' $argv
+    end
+end
+
+
+function !ping -d 'function of ping 8.8.8.8'
+    ping 8.8.8.8 $argv
+end
