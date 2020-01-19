29c11124a2b72bb99ea979fdc336e6069fd21be8
iory <ab.ioryz@gmail.com>
2020-01-19 20:53:32 +0900

    [fish] Delete linuxbrew

--- config/fish/conf.d/000-env.fish
+++ config/fish/conf.d/000-env.fish
@@ -3,7 +3,6 @@ if test -e ~/.fishrc.local
 end
 
 set -x PATH $HOME/.local/bin $PATH
-. ~/.config/fish/functions/linuxbrew.fish
 
 # automatically install fisher
 . ~/.config/fish/functions/fisher-extend.fish
